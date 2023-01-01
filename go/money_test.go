695e31bb594fafdb1fe0d50df5a227ba57c0383d
Daniel Underwood <cdanielunderwood@gmail.com>
2023-01-01 09:40:01 -0600

    fix: corrected broken TestMoney class

--- go/money_test.go
+++ go/money_test.go
@@ -14,6 +14,26 @@ func TestMultiplication(t *testing.T) {
 	}
 }
 
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	twentyEuros := tenEuros.Times(2)
+	if twentyEuros.amount != 20 {
+		t.Errorf("Expected 20, got: [%d]", twentyEuros.amount)
+	}
+	if twentyEuros.currency != "EUR" {
+		t.Errorf("Expected EUR, got: [%s]", twentyEuros.currency)
+	}
+}
+
+type Money struct {
+	amount   int
+	currency string
+}
+
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount * multiplier, currency: m.currency}
+}
+
 type Dollar struct {
 	amount int
 }
--- py/test_money.py
+++ py/test_money.py
@@ -5,7 +5,10 @@ class Dollar:
         self.amount = amount
 
     def times(self, multiplier):
-        return Dollar(self.amount, multiplier)
+        return Dollar(self.amount * multiplier)
+
+class TestMoney(unittest.TestCase):
+    def testMultiplication(self):
         fiver = Dollar(5)
         tenner = fiver.times(2)
         self.assertEqual(10, tenner.amount)
