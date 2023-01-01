6b6907da2e706fe44c3f1be4047a0a4b6ce66e40
Daniel Underwood <cdanielunderwood@gmail.com>
2023-01-01 13:13:09 -0600

    feat: addition feature for Moneys in same currency done

--- go/money_test.go
+++ go/money_test.go
@@ -26,6 +26,21 @@ func TestDivision(t *testing.T) {
 
 }
 
+func TestAddition(t *testing.T) {
+	var portfolio Portfolio
+	var portfolioInDollars Money
+
+	fiveDollars := Money{amount: 5, currency: "USD"}
+	tenDollars := Money{amount: 10, currency: "USD"}
+	fifteenDollars := Money{amount: 15, currency: "USD"}
+
+	portfolio = portfolio.Add(fiveDollars)
+	portfolio = portfolio.Add(tenDollars)
+	portfolioInDollars = portfolio.Evaluate("USD")
+
+	assertEqual(t, fifteenDollars, portfolioInDollars)
+}
+
 type Money struct {
 	amount   float64
 	currency string
@@ -43,4 +58,21 @@ func assertEqual(t *testing.T, expectedResult Money, actualResult Money) {
 	if expectedResult != actualResult {
 		t.Errorf("Expected [%+v], got [%+v]", expectedResult, actualResult)
 	}
+}
+
+type Portfolio []Money
+
+func (p Portfolio) Add(money Money) Portfolio {
+	p = append(p, money)
+	return p
+}
+
+func (p Portfolio) Evaluate(currency string) Money {
+	total := 0.0
+	for _, m := range p {
+		total = total + m.amount
+	}
+	return Money{amount: total, currency: currency}
+
+	return Money{amount: 15, currency: "USD"}
 }
--- js/test_money.js
+++ js/test_money.js
@@ -15,9 +15,26 @@ class Money {
     }
 }
 
-let fiver = new Money(5, "USD");
-let tenner = new Money(10, "USD");
-assert.deepStrictEqual(fiver.times(2), tenner);
+class Portfolio {
+    constructor() {
+        this.moneys = [];
+    }
+    
+    add(...moneys) { 
+        this.moneys = this.moneys.concat(moneys);
+    }
+
+    evaluate(currency) {
+        let total = this.moneys.reduce((sum, money) => {
+            return sum + money.amount;
+        }, 0)
+        return new Money(total, currency);
+    }
+}
+
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
+assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
 
 
 let tenEuros = new Money(10, "EUR");
@@ -27,3 +44,9 @@ assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
 let originalMoney = new Money(4002, "KRW");
 let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
 assert.deepStrictEqual(originalMoney.divide(4), expectedMoneyAfterDivision);
+
+let fifteenDollars = new Money(15, "USD");
+let portfolio = new Portfolio();
+portfolio.add(fiveDollars, tenDollars);
+assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
+
