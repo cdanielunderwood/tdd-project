c91913dd8390108870a727b586a09845412ce436
Daniel Underwood <cdanielunderwood@gmail.com>
2023-01-01 09:08:29 -0600

    feat: first green test after adding times method to dollar

---
+++ py/test_money.py
@@ -0,0 +1,14 @@
+import unittest
+
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def times(self, multiplier):
+        return Dollar(self.amount, multiplier)
+        fiver = Dollar(5)
+        tenner = fiver.times(2)
+        self.assertEqual(10, tenner.amount)
+
+if __name__ == '__main__':
+    unittest.main()
